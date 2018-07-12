546b8336d12fb38a940f48ddba4c992de0a2e7c6
VivienPichon <vivien.pichon@gmail.com>
2018-07-12 09:12:59 +0200

    Adding date to AmazingFijiFunction
    AmazingFijiFunction now includes the date of last change for each picture that has been taken.

--- Scripts old versions/AmazingFijiFunctions_NoDate.R
+++ Scripts old versions/AmazingFijiFunctions_NoDate.R
@@ -3,11 +3,6 @@
 ## Copyright (C) 2018  Kuhlemeier-Lab
 ## License GPL-3 | File LICENSE
 
-pack<-installed.packages()
-if(any(pack[,1]=="tidyr")==FALSE){
-  install.packages("tidyr")
-}
-
 gettable<-function(getcolumn="Length", scale , columns ,
                    output = "" # if specified, saves the table in "<output>.csv"
                    ) {
@@ -55,21 +50,12 @@ gettable<-function(getcolumn="Length", scale , columns ,
     workrow<-nrow(workfile)
     refrow<-as.character(workfile$Label)
     filerow<-as.character(workfile$File)
-    ## Manages the date format to make it understandable to R
-    dateformat<-workfile[which(colnames(workfile)=="Date.Modif")]
-    daterow<-as.character(dateformat[,1])
-    concat<-subset(dateformat,dateformat[,1]!=0)
-    concat<-separate(concat, Date.Modif, c("week","b","d","time","CEST","Y"),sep=" " )
-    concat<-data.frame(paste(concat$b,concat$d,concat$Y))
-    daterow[daterow!=0]<-as.character(as.Date(concat[,1],"%b %d %Y"))
-    daterow<<-daterow
     processrow<-workfile[getcolumn]
     for (j in 1:workrow){
       if (is.na(refrow[j])){
         tablebase<-c(tablebase,format(processrow[j,1]*truescale,digits=2,nsmall=2))
       } else if (is.na(refrow[j])==FALSE&refrow[j]!="scale"&refrow[j]!="ND"){
         tablebase<-c(tablebase,filerow[j])
-        tablebase<-c(tablebase,daterow[j])
         tablebase<-c(tablebase,refrow[j])
         tablebase<-c(tablebase,format(processrow[j,1]*truescale,digits=2,nsmall=2))
       } else if (refrow[j]=="ND"){
@@ -80,18 +66,16 @@ gettable<-function(getcolumn="Length", scale , columns ,
       }
     }
   }
-  tablebase<<-tablebase
-  lastcol<-length(columns)+2
-  allcolumns<-c("file","date",columns)
+  lastcol<-length(columns)+1
+  allcolumns<-c("file",columns)
   allcolumns<-factor(allcolumns,levels = unique (allcolumns)) ## turns characters into levelled factors to keep them in the right order when using split()
-  pre_df<-split(tablebase,allcolumns) ##splits the data into different columns
-  for (k in 4:lastcol){
+  pre_df<-split(tablebase,allcolumns)##splits the data into different columns
+  for (k in 3:lastcol){
     pre_df[[k]]<-as.numeric(pre_df[[k]])
   }
   bigtable<-data.frame(pre_df)
   bigtable[,1]<-as.character(bigtable[,1])
-  bigtable[,2]<-as.Date(bigtable[,2])
-  bigtable[,3]<-as.character(bigtable[,3])
+  bigtable[,2]<-as.character(bigtable[,2])
   # save to .csv if requested
   if (nchar(output) > 0) {
     output <- paste0(output, ".csv")
